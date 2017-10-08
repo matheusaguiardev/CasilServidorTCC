b25a2330ea042ed83f5a53574856f214ec2d1871
matheusaguiardev <joaomatheusaguiar@gmail.com>
2017-10-07 22:52:02 -0300

    feed de msg

--- ringer/index.js
+++ ringer/index.js
@@ -2,15 +2,17 @@ var gpio = require('rpi-gpio');
 var play = require('play').Play();
 
 const PIN_NUMBER = 7;
-var timeout = null;
-
+var timeout = null
+		console.log("Configuração de escuta do pino: " + PIN_NUMBER);
     gpio.on('change', function(channel, value){
       console.log('channel '+ channel + 'ding '+ value);
       press();
     });
         
     function press() {
+			console.log("Botão foi pressionado");
       if(!timeout) {
+				console.log(">>>>>>>>>>>> DING DING DING <<<<<<<<<<<<");
          play.sound(require('path').join(process.cwd(), './ding.wav'));
          timeout = setTimeout(function() {
          clearTimeout(timeout);
@@ -21,4 +23,4 @@ var timeout = null;
     }
 
 gpio.setup(PIN_NUMBER, gpio.DIR_IN, gpio.EDGE_BOTH);
-console.log('started');
+console.log('>>>>> Servidor iniciado <<<<<');
